07c659b36bfa930f60f2ae2d3bb79bb836050d8f
uynet <highsate@gmail.com>
2018-06-06 00:13:25 +0900

    css

--- src/main.js
+++ src/main.js
@@ -1,20 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-const createApp = _=>{
-  return new Vue({
-    e1: "#app",
-    data:{
-      message:"Hello unko"
-    }
-  })
-}
 
-if(typeof window !== "Undefined"){
-  createApp().$mount("#app")
-}
-else if(typeof module !== "undefined" && module.exports){
-  module.exports = createApp;
-}
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<div class="box">{{ todo.text }}</div>'
+})
+
+var app7 = new Vue({
+  el: '#app',
+  data: {
+    menu: [
+      { id: 0, text: 'About' },
+      { id: 1, text: "作ったもの" },
+      { id: 1, text: "経歴" },
+    ]
+  },
+  methods: {
+    a: function () {
+      this.menu[0].text = "こんにちは"
+      this.menu[1].text = "ぽきた"
+      this.menu[2].text = "ぽやしみ"
+      this.menu[3].text = "ぽはよう"
+    }
+  }
+})
+
 
 
